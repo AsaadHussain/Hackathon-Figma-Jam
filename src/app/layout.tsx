ad52a37a9b3ebbe9f20f1b88fe84722142a62aa9
AsaadHussain <asaadhussain2014@gmail.com>
2024-12-09 23:15:21 +0500

    responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,11 +11,6 @@ const pops = Poppins({
   variable: '--font-poppins'
 })
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
