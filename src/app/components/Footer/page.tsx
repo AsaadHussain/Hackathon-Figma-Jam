f167eab28944e2d501a0e1aae6a1690111b87631
AsaadHussain <asaadhussain2014@gmail.com>
2024-12-09 23:10:19 +0500

    responsive

--- src/app/components/Footer/page.tsx
+++ src/app/components/Footer/page.tsx
@@ -1,22 +1,17 @@
-
 export default function Footer() {
     return (
         <>
-            <div className="flex flex-col justify-between items-center pt-24 pb-10 h-16 bg-white w-full" >
+            <div className="flex flex-col justify-between items-center pt-24 pb-10 h-auto bg-white w-full">
 
-                <div className="flex justify-evenly items-center w-11/12">
+                <div className="flex flex-col sm:flex-row justify-evenly items-center w-11/12">
 
-                    <div className="flex flex-col justify-center items-start w-[23%] font-[400] text-[16px]" style={{ color: '#9f9f9f' }}>
-                        <p>
-                            400 University Drive Suite 200 Coral Gables,
-                        </p>
-                        <p>
-                            FL 33134 USA
-                        </p>
+                    <div className="flex flex-col justify-center items-start sm:w-[23%] w-full font-[400] text-[16px]" style={{ color: '#9f9f9f' }}>
+                        <p>400 University Drive Suite 200 Coral Gables,</p>
+                        <p>FL 33134 USA</p>
                     </div>
 
-                    <div className="flex justify-evenly items-start w-[77%]">
-                        <div className="poppins font-[500] text-[16px] flex flex-col justify-center items-start">
+                    <div className="flex flex-col sm:flex-row justify-evenly items-start sm:w-[77%] w-full">
+                        <div className="poppins font-[500] text-[16px] flex flex-col justify-center items-start sm:w-1/3 w-full">
                             <h3 className="pb-10" style={{ color: '#9f9f9f' }}>Links</h3>
                             <ul className=" flex flex-col font-[500] text-[16px] text-black">
                                 <li className="pb-8 hover:text-gray-300 cursor-pointer">Home</li>
@@ -25,7 +20,8 @@ export default function Footer() {
                                 <li className="pb-8 hover:text-gray-300 cursor-pointer">Contact</li>
                             </ul>
                         </div>
-                        <div className="font-[500] text-[16px] poppins flex flex-col justify-center items-start">
+
+                        <div className="font-[500] text-[16px] poppins flex flex-col justify-center items-start sm:w-1/3 w-full">
                             <h3 className="pb-10" style={{ color: '#9f9f9f' }}>Help</h3>
                             <ul className="flex flex-col justify-end text-black">
                                 <li className="pb-8 hover:text-gray-300 cursor-pointer">Payment Options</li>
@@ -33,10 +29,11 @@ export default function Footer() {
                                 <li className="pb-8 hover:text-gray-300 cursor-pointer">Privacy Policy</li>
                             </ul>
                         </div>
-                        <div className="poppins flex flex-col justify-center items-start">
+
+                        <div className="poppins flex flex-col justify-center items-start sm:w-1/3 w-full">
                             <h3 className="pb-10 font-[500] text-[16px]" style={{ color: '#9f9f9f' }}>Newsletter</h3>
                             <div className="flex justify-center text-black">
-                                <input className="w-[200px] pb-1 border-b-[2px] border-black font-[400] text-[14px]" type="text" placeholder="Enter Your Email Address" />
+                                <input className="w-full sm:w-[200px] pb-1 border-b-[2px] border-black font-[400] text-[14px]" type="text" placeholder="Enter Your Email Address" />
                                 <div className="pl-4 poppins font-[500] text-[14px]">
                                     <button className="border-b-[2px] border-black pb-1">
                                         SUBSCRIBE
@@ -51,12 +48,10 @@ export default function Footer() {
                 <hr className="bg-gray-300 w-11/12 pb-[2px] my-10" />
 
                 <div className="poppins font-[400] text-[16px] w-11/12 flex justify-start items-center pb-10">
-                    <p>
-                        2022 Meubel House. All rights reverved
-                    </p>
+                    <p>2022 Meubel House. All rights reserved</p>
                 </div>
 
             </div>
         </>
-    )
+    );
 }
--- src/app/components/Navbar/page.tsx
+++ src/app/components/Navbar/page.tsx
@@ -1,32 +1,49 @@
-   
-   "use client"
+"use client"
 
-   import Link from "next/link"
+import Link from "next/link"
 
-   export default function Navbar() {
-        return (
-            <>
-                <div className="flex justify-between items-center pt-12 pb-10 h-16" style={{ backgroundColor: "#fbebb5" }}>
-                    <div className="px-10 w-1/3"></div>
-                    <div className="p-10 w-1/3 ">
-                        <ul className="poppins flex space-x-14 font-[500] text-[16px] text-black">
-                            <li className="px-2 py-1 hover:text-gray-300 cursor-pointer"><Link href="/">Home</Link></li>
-                            <li className="px-2 py-1 hover:text-gray-300 cursor-pointer"><Link href="/shop">Shop</Link></li>
-                            <li className="px-2 py-1 hover:text-gray-300 cursor-pointer"><Link href="/about">About</Link></li>
-                            <li className="px-2 py-1 hover:text-gray-300 cursor-pointer"><Link href='/contact'>Contact</Link></li>
-                        </ul>
-                    </div>
-                    <div className="pr-20 w-1/3">
-                        <ul className="flex justify-end space-x-8 text-black">
-                            <li className="px-2 py-1 hover:text-gray-300 cursor-pointer"><i className="fa-solid fa-user-plus"></i></li>
-                            <li className="px-2 py-1 hover:text-gray-300 cursor-pointer"><i className="fa-solid fa-magnifying-glass"></i></li>
-                            <li className="px-2 py-1 hover:text-gray-300 cursor-pointer"><i className="fa-regular fa-heart"></i></li>
-                            <li className="px-2 py-1 hover:text-gray-300 cursor-pointer"><i className="fa-solid fa-cart-shopping"></i></li>
-                        </ul>
-                    </div>
+export default function Navbar() {
+    return (
+        <>
+            <div className="flex justify-between items-center pt-12 pb-10 h-16" style={{ backgroundColor: "#fbebb5" }}>
+               
+                <div className="px-10 w-1/3"></div>
 
+                <div className="p-10 md:p-2 w-full lg:w-1/3">
+                    <ul className="poppins hidden lg:flex sm:flex md:flex md:space-x-0 lg:space-x-12 s font-[500] text-[16px] text-black lg:justify-center">
+                        <li className="px-2 py-1 hover:text-gray-300 cursor-pointer">
+                            <Link href="/">Home</Link>
+                        </li>
+                        <li className="px-2 py-1 hover:text-gray-300 cursor-pointer">
+                            <Link href="/shop">Shop</Link>
+                        </li>
+                        <li className="px-2 py-1 hover:text-gray-300 cursor-pointer">
+                            <Link href="/about">About</Link>
+                        </li>
+                        <li className="px-2 py-1 hover:text-gray-300 cursor-pointer">
+                            <Link href='/contact'>Contact</Link>
+                        </li>
+                    </ul>
                 </div>
 
-            </>
-        )
-    }
+               
+                <div className="pr-20 w-full lg:w-1/3">
+                    <ul className="flex lg:justify-end lg:items-center sm:justify-center md:space-x-2 lg:space-x-7 text-black">
+                        <li className="px-2 py-1 hover:text-gray-300 cursor-pointer">
+                            <Link href="/account"><i className="fa-solid fa-user-plus"></i></Link>
+                        </li>
+                        <li className="px-2 py-1 hover:text-gray-300 cursor-pointer">
+                            <i className="fa-solid fa-magnifying-glass"></i>
+                        </li>
+                        <li className="px-2 py-1 hover:text-gray-300 cursor-pointer">
+                            <i className="fa-regular fa-heart"></i>
+                        </li>
+                        <li className="px-2 py-1 hover:text-gray-300 cursor-pointer">
+                            <Link href="/checkout"><i className="fa-solid fa-cart-shopping"></i></Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+}
